21c5586b51e3288b1527ce3e9dd7e7e77c1e79bb
Dan Simpson <dan.simpson@gmail.com>
2013-11-20 21:37:58 -0800

    Express setup

--- server.js
+++ server.js
@@ -3,7 +3,12 @@ var redis = require("redis"),
     temp_bus = new Events.EventEmitter(),
     redis_client = redis.createClient(6379, "192.168.0.2");
 
-temp_bus.addListener("update", function () {
+var express = require('express');
+var routes = require('./routes');
+var http = require('http');
+var path = require('path');
+
+temp_bus.on("update", function () {
   console.log("emitter update!", this, arguments);
 })
 
@@ -19,4 +24,29 @@ redis_client.on("message", function (channel, message) {
 
 redis_client.on("ready", function () {
   redis_client.subscribe("temperature");
+});
+
+// all environments
+var app = express();
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(app.router);
+// app.use(require('stylus').middleware(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+app.get('/', routes.index);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
 });
